693ab553e53303b0ac802b11d13ee0ffa3d04473
Malthorn1 <mikkelskott@gmail.com>
2020-04-15 14:36:02 +0200

    pokes stuff

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+
 
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,55 +7,52 @@ import facade from "./apiFacade";
 
 export default Pokemon;
 
-
-function Pokemon(){
-  const[pokemonName, setPokemonName] = useState();
-  const[pokemonID, setPokemonID] = useState();
-  const[pokemonType, setPokemonType] = useState();
-  const[pokemonMove, setPokemonMove] = useState();
-
+function Pokemon() {
+  const [pokemonName, setPokemonName] = useState();
+  const [pokemonID, setPokemonID] = useState();
+  const [pokemonType, setPokemonType] = useState();
+  const [pokemonMove, setPokemonMove] = useState();
 
 
 
-  function getPokemon(){
-      let options = {
-        'method': 'GET',
-        'headers': {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      }
-      fetch("http://localhost:8080/CA3/api/pokemon/1", options)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        setPokemonName(data.name); 
-        setPokemonID(data.id)
-  
-      },[]);
-    }
+  function getPokemon() {
+    let options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    };
+    fetch("http://localhost:8080/CA3/api/pokemon/1", options)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemonName(data.name);
+        setPokemonID(data.id);
+        setPokemonType(data.types[0].type.name);
+        console.log(data)
+      }, []);
+  }
 
-    function pokemonfact() {
-      return (pokemonName ? (<>
-          <h1>Pokemon info:</h1>
-          <h4>Pokemon ID: {pokemonID}</h4>
-          <h4>Pokemon Name: {pokemonName}</h4>
-        
+  function pokemonfact() {
+    
+    return pokemonName ? (
+      <>
+        <h1>Pokemon info:</h1>
+        <h4>Pokemon ID: {pokemonID}</h4>
+        <h4>Pokemon Name: {pokemonName}</h4>
+        <h4>Pokemon Type: {pokemonType}</h4>
+      </>
+    ) : (
+      <></>
+    );
+  }
 
+  return (
+    <div className="App">
+      {pokemonfact()}
+      <br />
 
-   }
-          </>):(<></>));
-        }
-
-
-        return (
-          <div className="App">
-              
-            {pokemonfact()}<br/>
-            
-          
-       
-            <button onClick={getPokemon}>Get Pokemon</button>
-          </div>
-        );
-        }
+      <button onClick={getPokemon}>Get Pokemon</button>
+    </div>
+  );
+}
