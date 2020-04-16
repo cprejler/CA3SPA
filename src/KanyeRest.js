848cd5ff7599b216a14bdffc887915a9fc50c86b
Plebbz <jens.ohlendorff@gmail.com>
2020-04-16 10:37:01 +0200

    kanyerest js

--- src/KanyeRest.js
+++ src/KanyeRest.js
@@ -5,10 +5,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import facade from "./apiFacade";
 
 export default function KanyeRest(){
+    const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = fetch("http://localhost:8080/CA3/api/kanye")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log("UPPS"));
+  }, []);
     return(
-        <div>This is a fact fact</div>
-
-    )
-        
-    
+      <div className="container">
+      <div  className="jumbotron">
+        <h1 className="display-4 text-primary">This is a Kanye Quote</h1>
+        <p  className="font-weight-bold">{data.text}</p>
+      </div>
+    </div>
+    );
 }
